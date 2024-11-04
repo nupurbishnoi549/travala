568e0f2228a7f7569cf893a62ab24184d2325f5c
Your Name <you@example.com>
2024-11-04 09:46:34 -0800

    change swiper into slider

--- assets/JS/slider.js
+++ assets/JS/slider.js
@@ -1,7 +1,50 @@
-var swiper = new Swiper(".mySwiper", {
-    direction: "vertical",
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
+$('.slick-Card').slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $('.custom-prev'), 
+    nextArrow: $('.custom-next'),
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 769,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+$('.slick-vertical').slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    vertical: true,
+    verticalSwiping: true,
+    prevArrow: $('.custom-prev-vertical'),
+    nextArrow: $('.custom-next-vertical'),
+    responsive: [
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
 });
