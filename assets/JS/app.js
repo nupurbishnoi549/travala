2a30c62b59f645966e4cdfcc31db455f67bda456
BimlaJain <vj13798@gmail.com>
2024-11-05 14:48:00 +0530

    add video in slider

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -77,3 +77,15 @@ mybutton.addEventListener("click", function () {
         behavior: 'smooth'
     });
 });
+const videoElement = document.querySelector('.video');
+const videoIcon = document.querySelector('.icon');
+
+videoElement.addEventListener('play', () => {
+    videoIcon.classList.add('hidden'); // Hide icon when video starts
+});
+
+videoElement.addEventListener('pause', () => {
+    videoIcon.classList.remove('hidden'); // Show icon when video pauses
+});
+
+
